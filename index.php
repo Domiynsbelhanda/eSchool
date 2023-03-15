4b0ffae32acf632eb1fa999f31b00bd76453e285
Youness Dominique <youness.dominique@gmail.com>
2023-03-15 13:21:26 +0200

    [FEAT] Project setting up

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--    Bootstrap CSS-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!--    Font Awesome CSS-->
+    <link rel="stylesheet" href="css/all.min.css">
+
+    <title>eSchool</title>
+</head>
+<body>
+
+<!--Font Awesome JS-->
+<script src="js/all.min.js"></script>
+
+<!--JQuery Plugin-->
+<script src="js/jquery.min.js"></script>
+
+<!--Bootstrap JS-->
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
