3deb91cd51ad726590ea78f5788b3426cdb09550
Youness Dominique <youness.dominique@gmail.com>
2023-03-15 13:52:48 +0200

    [STYLE] Update navbar item element

--- index.php
+++ index.php
@@ -17,68 +17,71 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik+Iso&display=swap" rel="stylesheet">
 
+    <!--    Custom Css -->
+    <link rel="stylesheet" href="css/style.css">
+
     <title>eSchool</title>
 </head>
 <body>
 <!--Start Navigation-->
-    <nav class="navbar navbar-expand-sm bg-body-tertiary">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php" style="font-family: 'Rubik Iso', cursive;">eSchool</a>
+            <a class="navbar-brand" href="index.php">eSchool</a>
             <span class="navbar-text">Apprendre et mettre en œuvre</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
+                <ul class="navbar-nav custom-nav">
+                    <li class="nav-item custom-nav-item">
                         <a href="index.php" class="nav-link">
                             Accueil
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Cours
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Paiement
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Profil
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Deconnexion
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Connexion
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Inscription
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             FeedBack
                         </a>
                     </li>
 
-                    <li class="nav-item">
+                    <li class="nav-item custom-nav-item">
                         <a href="#" class="nav-link">
                             Contact
                         </a>
