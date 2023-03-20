887d4c8066940fa80e6375109fa1bf1a024287b8
Youness Dominique <youness.dominique@gmail.com>
2023-03-20 11:37:30 +0200

    [FEAT] Connect login page

--- footer.php
+++ footer.php
@@ -45,14 +45,14 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
+        <form action="settings/login.php" method="post">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Adresse email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" id="password" name="password">
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
--- settings/login.php
+++ settings/login.php
@@ -2,13 +2,14 @@
     include('./database.php');
     session_start();
 
-    if ( !isset($_POST['username'], $_POST['password'], $_POST['email']) ) {
+    if ( !isset($_POST['password'], $_POST['email']) ) {
         // Could not get the data that should have been sent.
         exit('Please fill both the username and password fields!');
     }
 
+    // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
     if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) {
-        // Bind parameters (s = string, i = int, b = blob, etc), in our case the email is a string so we use "s"
+        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
         $stmt->bind_param('s', $_POST['email']);
         $stmt->execute();
         // Store the result so we can check if the account exists in the database.
@@ -24,7 +25,7 @@
                 // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
                 session_regenerate_id();
                 $_SESSION['loggedin'] = TRUE;
-                $_SESSION['name'] = $_POST['username'];
+                $_SESSION['name'] = $_POST['email'];
                 $_SESSION['id'] = $id;
                 echo 'Welcome ' . $_SESSION['name'] . '!';
             } else {
@@ -35,10 +36,7 @@
             // Incorrect username
             echo 'Incorrect username and/or password!';
         }
-    
-    
+        
         $stmt->close();
     }
-
-    print($_POST['username']);
 ?>
