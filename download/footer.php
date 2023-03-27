c24bbbb8872c210fee976cfe8df3f84f45f21b0b
Youness Dominique <youness.dominique@gmail.com>
2023-03-27 10:29:05 +0200

    [FEAT] migrate project

--- download/footer.php
+++ download/footer.php
@@ -1,5 +1,23 @@
 <html>
 <head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="eLearning is a modern and fully responsive Template by WebThemez.">
+	<meta name="author" content="webThemez.com">
+	<title>eLearning - Free Educational Responsive Web Template </title>
+	<link rel="favicon" href="../assets/images/favicon.png">
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="../assets/css/bootstrap-theme.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="../assets/css/da-slider.css" />
+	<link rel="stylesheet" href="../assets/css/style.css">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
 </head>
 <body>
 <!-- container -->
@@ -42,9 +60,7 @@
 						<div class="panel-body">
 							<p class="simplenav">
 								<a href="index.php">Home</a> | 
-								<a href="login.php">Login</a> | 
-								<a href="login.php">Courses</a> |
-								<a href="contact.php">Feedback</a>
+								<a href="../index.php">Logout</a> | 
 							</p>
 						</div>
 					</div>
--- user/courses.php
+++ user/courses.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="eLearning is a modern and fully responsive Template by WebThemez.">
 	<meta name="author" content="webThemez.com">
-	<title>eLearning - Free Educational Responsive Website</title>
+	<title>About - Techro Bootstrap template</title>
 	<link rel="favicon" href="assets/images/favicon.png">
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 	<!-- Custom styles for our template -->
 	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
-	<link rel="stylesheet" type="text/css" href="assets/css/da-slider.css" />
 	<link rel="stylesheet" href="assets/css/style.css">
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
@@ -20,50 +19,42 @@
 	<script src="assets/js/respond.min.js"></script>
 	<![endif]-->
 </head>
+
 <body>
 	<?php
 		include "nav.php";
-		/*if(isset($_SESSION["username"]))
-		{
-			//$username=$_SESSION["username"];
-		}
-		else{ echo 'session not started';}*/
-
+		$strconn=mysqli_connect("localhost","root","","project");
+		if(!$strconn)
+			echo "Connection failed".mysqli_connect_error();
+		else{}
 	?>
-
-	<!-- Header -->
-	<header id="head">
+	<?php
+		$query = "SELECT * FROM subject LIMIT 1";
+		$result = mysqli_query($strconn,$query);
+		/*if($result)
+		{
+			echo "Sucess";
+		}
+		else{
+			echo "failed";
+		}*/
+	?>
+	<header id="head" class="secondary">
 		<div class="container">
-			<div class="banner-content">
-				<div id="da-slider" class="da-slider">
-					<div class="da-slide">
-						<h2>Online Education</h2>
-						<p>The purpose is to teach</p>
-						<div class="da-img"></div>
-					</div>
-					<div class="da-slide">
-						<h2></h2>
-						<p></p>
-						<div class="da-img"></div>
-					</div>
-					<div class="da-slide">
-						<h2></h2>
-						<p></p>
-						<div class="da-img"></div>
-					</div>
+			<div class="row">
+				<div class="col-sm-8">
+					<h1>Courses</h1>
 				</div>
 			</div>
 		</div>
 	</header>
-	<!-- /Header -->
-
 	<div id="courses">
-		<div class="container">
+		<section class="container">
 			<h2>Available Courses</h2>
 			<div class="row">
 				<div class="col-md-4">
 					<div class="featured-box">
-					<a href="login.php">
+					<a href="HTML.php">
 						<i class="fa fa-cogs fa-2x"></i>
 						<div class="text">
 							<h3>HTML</h3>
@@ -74,7 +65,7 @@
 				</div>
 				<div class="col-md-4">
 					<div class="featured-box">
-					<a href="login.php">
+					<a href="CSS.php">
 						<i class="fa fa-leaf fa-2x"></i>
 						<div class="text">
 							<h3>CSS</h3>
@@ -83,17 +74,31 @@
 					</a>
 					</div>
 				</div>
-
-		</div>
+				<?php
+				while($row = mysqli_fetch_array($result))
+				{
+					echo '<div class="col-md-4">';
+					echo '<div class="featured-box">';
+					echo '<a href="new.php">';
+					echo '<i class="fa fa-leaf fa-2x"></i>';
+					echo '<div class="text">';
+					echo '<h3>'.$row[0].'</h3>';
+					echo $row[1];
+					echo '</div>';
+					echo '</a>';
+					echo '</div>';
+					echo '</div>';
+				}
+				?>
+			</div>
+		</section>
 	</div>
 	<?php
 		include "footer.php";
 	?>
-<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
-	<script src="assets/js/modernizr-latest.js"></script>
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-	<script src="assets/js/jquery.cslider.js"></script>
 	<script src="assets/js/custom.js"></script>
 </body>
 </html>
