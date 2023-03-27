aad76799f5ff86b1629face38685259fbe3c887e
Youness Dominique <youness.dominique@gmail.com>
2023-03-27 11:13:34 +0200

    [STYLE] Translate contact page

--- contact.php
+++ contact.php
@@ -1,23 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="eLearning is a modern and fully responsive Template by WebThemez.">
-	<meta name="author" content="webThemez.com">
-	<title>Feedback </title>
-	<link rel="favicon" href="assets/images/favicon.png">
-	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
-	<!-- Custom styles for our template -->
-	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
-	<link rel="stylesheet" href="assets/css/style.css">
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="assets/js/html5shiv.js"></script>
-	<script src="assets/js/respond.min.js"></script>
-	<![endif]-->
+	<?php
+		include "head.php";
+	?>
 </head>
 
 <body>
@@ -36,7 +22,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-8">
-					<h1>Feedback</h1>
+					<h1>Contact</h1>
 				</div>
 			</div>
 		</div>
@@ -46,50 +32,50 @@
 	<div class="container">
 				<div class="row">
 					<div class="col-md-6">
-						<h3 class="section-title">Your Message</h3>
+						<h3 class="section-title">Votre message</h3>
 			<form name="sentMessage" id="contactForm"  novalidate method="post" action=""> 
-			<div class="control-group">
-			<div class="controls">
-			<input type="text" class="form-control" 
-			placeholder="Full Name" name="name" required
-			data-validation-required-message="Please enter your name" />
-			<p class="help-block"></p>
-			</div>
-			</div> 	
-			<div class="control-group">
-			<div class="controls">
-			<input type="email" class="form-control" placeholder="Email" 
-			name="email" required
-			data-validation-required-message="Please enter your email" />
-			</div>
-			</div> 	
-			<div class="control-group">
-			<div class="controls">
-			<textarea rows="10" cols="100" class="form-control" 
-			placeholder="Message" name="message" required
-			data-validation-required-message="Please enter your message" minlength="5" 
-			data-validation-minlength-message="Min 5 characters" 
-			maxlength="999" style="resize:none;margin-top:10px"></textarea>
-			</div>
-			</div> 	
-			<br>
-			<div id="success"> 
-		<?php
-		if(isset($_POST['btnsubmit']))
-		{
-			$name=$_POST['name'];
-			$email=$_POST['email'];
-			$message=$_POST['message'];
-			$strInsert="INSERT INTO feedback(Description,name,email) VALUES('$message','$name','$email')";
-			if(mysqli_query($strconn,$strInsert))
-				echo "<div class='alert alert-success' role='alert'>Thanks for feedback.</div>";
-			else
-					echo "<div class='alert alert-danger' role='alert'>something wrong.</div>";
+					<div class="control-group">
+					<div class="controls">
+					<input type="text" class="form-control" 
+					placeholder="Nom complet" name="name" required
+					data-validation-required-message="Veuillez entrer votre nom complet" />
+					<p class="help-block"></p>
+					</div>
+					</div> 	
+					<div class="control-group">
+					<div class="controls">
+					<input type="email" class="form-control" placeholder="Email" 
+					name="email" required
+					data-validation-required-message="Veuillez entrer votre email" />
+					</div>
+					</div> 	
+					<div class="control-group">
+					<div class="controls">
+					<textarea rows="10" cols="100" class="form-control" 
+					placeholder="Message" name="message" required
+					data-validation-required-message="Veuillez entrer votre message" minlength="5" 
+					data-validation-minlength-message="Min 5 characters" 
+					maxlength="999" style="resize:none;margin-top:10px"></textarea>
+					</div>
+					</div> 	
+					<br>
+					<div id="success"> 
+				<?php
+				if(isset($_POST['btnsubmit']))
+				{
+					$name=$_POST['name'];
+					$email=$_POST['email'];
+					$message=$_POST['message'];
+					$strInsert="INSERT INTO feedback(Description,name,email) VALUES('$message','$name','$email')";
+					if(mysqli_query($strconn,$strInsert))
+						echo "<div class='alert alert-success' role='alert'>Merci de nous avoir contacté.</div>";
+					else
+							echo "<div class='alert alert-danger' role='alert'>Erreur dans l'envoie.</div>";
 
-		}
-		?>
-		</div> <!-- For success/fail messages -->
-			<button type="submit" class="btn btn-primary pull-right" name="btnsubmit" >Send</button><br /><br /><br /><br />
+				}
+				?>
+				</div> <!-- For success/fail messages -->
+					<button type="submit" class="btn btn-primary pull-right" name="btnsubmit" >Envoyé</button><br /><br /><br /><br />
 			</form>
 			</div>
 			</div>
